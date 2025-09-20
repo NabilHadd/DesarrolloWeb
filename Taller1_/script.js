cf3526a3f413867b601888295c2a026f8005f9ca
Nabil Ignacio Haddad Pomar <nabil.haddad@alumnos.ucn.cl>
2025-09-20 00:33:07 -0300

    avances responsive

--- Taller1_/script.js
+++ Taller1_/script.js
@@ -1,16 +1,59 @@
+const visibility = [false, false, false, false]
 
 
-fetch('https://pokeapi.co/api/v2/pokemon?offset=20&limit=100')
+const contents = document.querySelectorAll(".content");
+contents.forEach(content=> {
+    content.style.display = 'none'
+})
+
+
+
+//Reconocimiento de que se le esta haciendo click a cada una de los apartados, servira despues para abrirlos y cerrarlos cuando sea necesario.
+const items = document.querySelectorAll('.button');
+
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        const container = item.parentElement.querySelector(".content");
+
+        if(visibility[Number(item.id)]){
+            container.style.display = 'none';
+            visibility[Number(item.id)] = false;
+        }else{
+            container.style.display = 'block'
+            visibility[Number(item.id)] = true;
+        }
+    })
+})
+//Esta diseñado de esta manera debido a que en el taller dice "Navegar a una vista detallada por cada recurso, mostrando información ampliada al seleccionarlo."
+// y "los usuarios puedan explorar los datos sin necesidad de recargar la página."
+
+
+
+
+//Consulta pokemons
+fetch('https://pokeapi.co/api/v2/pokemon?offset=20&limit=1000')
 .then(res => {
 if (!res.ok) throw new Error(`HTTP ${res.status}`);
 return res.json(); // o res.text(), res.blob()
 })
 .then(data => {
     for (let index = 0; index < 4; index++) {
+        const num = Math.floor(Math.random() * data.results.length)
         const p = document.createElement('p');
-        p.textContent = data.results[ Math.floor(Math.random() * data.results.length)].name
+        const pokemon_url = data.results[num].url
+        p.textContent = data.results[num].name
         document.getElementById(`pokemon_${index + 1}`).appendChild(p)
+        fetch(pokemon_url)
+        .then(res => {
+            if(!res.ok) throw new Error(`HTTP ${res.status}`);
+            return res.json();
+        }).then(data => {
+            const html_img = document.createElement('img');
+            html_img.src = data.sprites.front_default;
+            document.getElementById(`pokemon_${index + 1}`).appendChild(html_img)
+        })
     }
     
 })
 .catch(err => console.error('Error:', err));
+//Fin consulta pokemons.
