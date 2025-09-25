c4c9c2efbb6dc38a8655345f95b87cf399df61ec
Nabil Ignacio Haddad Pomar <nabil.haddad@alumnos.ucn.cl>
2025-09-25 15:24:58 -0300

    Update script.js

--- Taller1_/script.js
+++ Taller1_/script.js
@@ -157,7 +157,7 @@ return res.json(); // o res.text(), res.blob()
     items.map(i=> {
         const div = document.createElement('div');
         div.id = i.codigo;
-        div.class = "rounded lg"
+        miElemento.classList.add('rounded lg');
         const pNombre = document.createElement('p');
         pNombre.textContent = i.nombre;
         div.appendChild(pNombre);
