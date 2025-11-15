331c43cd7cd0450f5d863f482516b7c30f7b668e
Nabil Ignacio Haddad Pomar <nabil.haddad@alumnos.ucn.cl>
2025-11-15 17:55:36 -0300

    seed Pokemon

--- Taller2_/seedPokemon.py
+++ Taller2_/seedPokemon.py
@@ -2,7 +2,6 @@ import psycopg2
 import pandas as pd
 import requests
 import io
-from datetime import datetime
 
 
 DB_CONFIG = {
@@ -15,6 +14,7 @@ DB_CONFIG = {
 URL = "https://pokeapi.co/api/v2/"
 ABILITY = 'ability/'
 TYPE = 'type/'
+POSTFIJO = '?limit=100000&offset=0'
 POKEMON = 'pokemon/'
 
 
@@ -25,19 +25,19 @@ def conectar_bd():
 
 
 def requestPokemon(pokemon):
-    response = requests.get(f'{URL+POKEMON}/{pokemon}')
+    response = requests.get(f'{URL+POKEMON}{pokemon}')
     raw_data = response.json()
     return {
         'id': raw_data.get('id'),
+        'name': raw_data.get('species').get('name'),
         'height': raw_data.get('height'),
         'weight': raw_data.get('weight'),
-        'name': raw_data.get('species').get('name'),
         'sprite': raw_data.get('sprites').get('front_default')
     }
 
 
 def requestType(type):
-    response = requests.get(f'{URL+POKEMON}/{type}')
+    response = requests.get(f'{URL+TYPE}{type}')
     raw_data = response.json()
     return {
         'id': raw_data.get('id'),
@@ -45,27 +45,59 @@ def requestType(type):
         'sprite': raw_data.get('sprites').get('generation-iii').get('colosseum').get('name_icon')
     }
 
+def requestAbility(ability):
+    response = requests.get(f'{URL}{ABILITY}{ability}')
+    raw_data = response.json()
+
+    # buscar efecto en inglés
+    effect_entries = raw_data.get('effect_entries', [])
+    effect = 'null'
+
+    for entry in effect_entries:
+        if entry.get('language', {}).get('name') == 'en':
+            effect = entry.get('short_effect')
+            break
+
+    return {
+        'id': raw_data.get('id'),
+        'name': raw_data.get('name'),
+        'effect': effect
+    }
+
+def requestPokemonType(pokemon):
+    response = requests.get(f'{URL+POKEMON}{pokemon}')
+    raw_data = response.json()
+    types = raw_data.get('types')
+    id_pokemon = raw_data.get('id')
+    return [{
+        'id_type': x
+                    .get('type')
+                    .get('name'),
+
+        'id_pokemon': id_pokemon
+        } 
+        for x in types
+        ]
+
+
+def requestPokemonAbility(pokemon):
+    response = requests.get(f'{URL+POKEMON}/{pokemon}')
+    raw_data = response.json()
+    abilities = raw_data.get('abilities')
+    id_pokemon = raw_data.get('id')
+    return [
+        {
+            'id_ability': x['ability']['url']
+                                    .rstrip('/')
+                                    .split('/')[-1],
+
+            'id_pokemon': id_pokemon
+        } 
+        for x in abilities
+        ]
 
 
 
-
-
-
-
-
-def requestSerie(item, date):
-    response = requests.get(f'{URL}/{item}/{date}')
-    respuesta = response.json()['serie']
-
-    for d in respuesta:
-        d['indicator_code'] = item
-        d['date'] = datetime.strptime(d['fecha'], "%Y-%m-%d").date()
-        d['value'] = d['valor']
-        d.pop('valor', None)
-        d.pop('fecha', None)
-
-
-    return respuesta
 
 
 
@@ -73,45 +105,88 @@ def main():
 
     
     #todos los item economicos
-    response = requests.get(URL)
-    indicator_names = list(response.json().keys())[3:]
+    types_response = requests.get(URL+TYPE+POSTFIJO)
+    type_names = map(lambda x : x.get('name') ,types_response.json().get('results'))
+    data_types = [requestType(x) for x in type_names]
 
-    indicator_list = list(response.json().items())[3:]
+    abilities_response = requests.get(URL+ABILITY+POSTFIJO)
+    abilitie_names = map(lambda x : x.get('name') ,abilities_response.json().get('results'))
+    data_abilities = [requestAbility(x) for x in abilitie_names]
 
-    for _, d in indicator_list:
-        d.pop("fecha", None)
-        d.pop("valor", None)
+    pokemon_response = requests.get(URL+POKEMON+POSTFIJO)
+    pokemon_names = map(lambda x : x.get('name') ,pokemon_response.json().get('results'))
+    data_pokemon = [requestPokemon(x) for x in pokemon_names]
+    data_pokemon_ability = [requestPokemonAbility(x) for x in pokemon_names]
+    data_pokemon_type = [requestPokemonType(x) for x in pokemon_names]
+
+
+
+    df_types = pd.DataFrame(data_types)
+
+    df_abilities = pd.DataFrame(data_abilities)
+
+    df_pokemons = pd.DataFrame(data_pokemon)
+
+    df_pokemon_ability = pd.DataFrame(data_pokemon_ability)
+
+    df_pokemon_type = pd.DataFrame(data_pokemon_type)
 
     
-    history_indicators_mtx = [requestSerie(i, YEAR) for i in indicator_names]
-    history_indicators_flat = [x for sub in history_indicators_mtx for x in sub]
-
-    # dataframe indicadores
-    data_indicators = [v for _, v in indicator_list]
-    df_indicators = pd.DataFrame(data_indicators)
-
-    # dataframe historicos
-    data_historic = [v for  v in history_indicators_flat]
-    df_historic = pd.DataFrame(data_historic)
 
     # conexión
     conn = psycopg2.connect(**DB_CONFIG)
     cur = conn.cursor()
 
-    # ---------- INSERT DF 1: indicator ----------
+    # ---------- INSERT DF 1: pokemon ----------
     buffer1 = io.StringIO()
-    df_indicators.to_csv(buffer1, index=False, header=False)
+    df_pokemons.to_csv(buffer1, index=False, header=False, sep='\t')
     buffer1.seek(0)
 
-    cur.copy_from(buffer1, 'indicator', sep=',')
+    cur.copy_from(buffer1, 'pokemon', sep='\t')
     conn.commit()
 
-    # ---------- INSERT DF 2: indicator_history ----------
+
+
+    # ---------- INSERT DF 2: types ----------
     buffer2 = io.StringIO()
-    df_historic.to_csv(buffer2, index=False, header=False)
+    df_types.to_csv(buffer2, index=False, header=False, sep='\t')
     buffer2.seek(0)
 
-    cur.copy_from(buffer2, 'indicator_value', sep=',')
+    cur.copy_from(buffer2, 'type', sep='\t')
+    conn.commit()
+    
+
+
+
+    # ---------- INSERT DF 3: abilities ----------
+    buffer3 = io.StringIO()
+    df_abilities.to_csv(buffer3, index=False, header=False, sep='\t')
+    buffer3.seek(0)
+
+    cur.copy_from(buffer3, 'ability', sep='\t')
+    conn.commit()
+
+
+
+
+    # ---------- INSERT DF 4: pokemon-type ----------
+    buffer4 = io.StringIO()
+    df_pokemon_type.to_csv(buffer4, index=False, header=False, sep='\t')
+    buffer4.seek(0)
+
+    cur.copy_from(buffer4, 'pokemon_type', sep='\t')
+    conn.commit()
+
+
+
+
+        # ---------- INSERT DF 5: pokemon-ability  ----------
+    buffer5 = io.StringIO()
+    df_pokemon_ability.to_csv(buffer5, index=False, header=False, sep='\t')
+    buffer5.seek(0)
+
+
+    cur.copy_from(buffer5, 'pokemon_ability', sep='\t')
     conn.commit()
 
     cur.close()
